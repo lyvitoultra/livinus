227233166ea042c8086b26c773a6e77f2fbc5fd1
lyvitoultra <livinus.nriaka@binghamuni.edu.ng>
2024-11-18 00:24:41 +0100

    Add files via upload

---
+++ livinus_chibueze_cos101 .py
@@ -0,0 +1,67 @@
+def calculate_power():
+    work=int(input('value of work'))
+    time=int(input('value of time'))
+    power=work/time
+
+
+    print('power_is',power)
+calculate_power()
+
+
+
+def calculate_density():
+    mass=int(input('value of mass'))
+    volume=int(input('value of volume'))
+    density=mass/volume
+
+
+
+    print('density_is',density)
+calculate_density()
+
+
+
+def calculate_force():
+    mass=int(input('value of mass'))
+    acceleration=int(input('value of acceleration'))
+    force=mass*acceleration
+
+
+
+    print('force_is',force)
+calculate_force()
+
+
+
+def calculate_area_of_rectangle():
+    length=int(input('value of length'))
+    breadth=int(input('value of breadth'))
+    area_of_rectangle=length*breadth
+
+
+
+    print('area_of_rectangle',area_of_rectangle)
+calculate_area_of_rectangle()
+
+
+
+def calculate_area_of_triangle():
+    base=int(input('value of base'))
+    height=int(input('value of height'))
+    two=int(input('value of two'))
+    area_of_triangle=base*height/two
+
+
+
+    print('area_of_triangle',area_of_triangle)
+calculate_area_of_triangle()
+
+
+
+
+
+
+
+
+
+
